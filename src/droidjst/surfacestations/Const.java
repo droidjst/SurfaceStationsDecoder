77a9ffa5cf526eafd15481b83f2a9749c3daeb6f
Joe <droidjst@gmail.com>
2015-09-29 09:24:51 -0400

    Project Constants
    This includes constants used throughout the program.

---
+++ src/droidjst/surfacestations/Const.java
@@ -0,0 +1,32 @@
+
+/*
+    Copyright 2015 Joseph Tranquillo {name of copyright owner}
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+ */
+
+
+package droidjst.surfacestations;
+
+import java.io.File;
+
+public class Const
+{
+    public final static String URL_NCAR_RAP_STATIONS = "http://weather.rap.ucar.edu/surface/stations.txt";
+    
+    public final static String STATIONS_FILE = "stations";
+    
+    public final static String DB_NAME = "stations.db";
+    
+    public final static String FILE_SEP = File.separator;
+}
