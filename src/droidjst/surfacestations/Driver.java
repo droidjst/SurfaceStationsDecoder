14afc6b28fa81efb4c33f86030f2eadf8220f445
Joe <droidjst@gmail.com>
2015-09-29 09:31:28 -0400

    The Driver
    This will download the stations to local text file and then begin to add
    them to an SQLite database.

---
+++ src/droidjst/surfacestations/Driver.java
@@ -0,0 +1,135 @@
+
+/*
+    Copyright 2015 Joseph Tranquillo {name of copyright owner}
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+ */
+
+
+package droidjst.surfacestations;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.sql.SQLException;
+
+public class Driver
+{
+    private static HTTPUtil httputil;
+    
+    private static Exception exception;
+    
+    private static void init()
+    {
+        httputil = new HTTPUtil();
+    }
+    
+    public static void main(String[] args)
+    {
+        init();
+        
+        String uri = new File("").getAbsolutePath() + Const.FILE_SEP + "stations";
+        
+        File file = new File(uri);
+        
+        if(file.exists() == false)
+        {
+            try
+            {
+                httputil.downloadWebpage(Const.URL_NCAR_RAP_STATIONS, file);
+            }
+            catch (MalformedURLException e)
+            {
+                exception = e;
+            }
+            catch (IOException e)
+            {
+                exception = e;
+            }
+            
+            if(exception != null)
+            {
+                System.err.println(exception.getMessage());
+                
+                System.exit(1);
+            }
+        }
+        
+        Database database = new Database();
+        
+        if(database.isAvailable() == false)
+        {
+            try
+            {
+                database.createSQLiteTable();
+            }
+            catch (SQLException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        
+        DatabaseUtil databaseutil = new DatabaseUtil();
+        
+        try
+        {
+            databaseutil.parseFileToSQLite();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+    }
+    
+    /*
+    @SuppressWarnings("unused")
+    private static void printHeaders(File file)
+    {
+        BufferedReader breader = null;
+        
+        try
+        {
+            breader = new BufferedReader(new FileReader(file), 16 * 1024);
+            
+            String line = null;
+            
+            while((line = breader.readLine()) != null)
+            {
+                if(line.matches(".*([\\d]{2}-[A-Z]{3}-[\\d]{2}[\\s]*)"))
+                {
+                    System.out.println(line);
+                }
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                breader.close();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+    */
+}
